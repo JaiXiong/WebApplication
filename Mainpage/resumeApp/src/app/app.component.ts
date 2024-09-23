ac96ec5a6e237465c662a7b733774283c95460b3
JaiLaptop <jai_1_23@hotmail.com>
2024-09-22 22:32:32 -0500

    all the work from today

--- Mainpage/resumeApp/src/app/app.component.ts
+++ Mainpage/resumeApp/src/app/app.component.ts
@@ -1,32 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgIf } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { MatTabsModule } from '@angular/material/tabs'; // Import MatTabsModule
+import { MatTab, MatTabGroup, MatTabsModule } from '@angular/material/tabs'; // Import MatTabsModule
 import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+//import { provideAnimations } from '@angular/platform-browser/animations';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', //this is the name of this component i guess? for now we name is app-root maybe name is app-mainpage later?
   standalone: true,
   imports: 
   [
     RouterOutlet, 
-    CommonModule, 
+    //CommonModule, 
     FormsModule,
     MatTabsModule,
     MatInputModule,
+    MatTabGroup,
+    MatTab,
+    NgIf,
+    MatIconModule,
     //BrowserAnimationsModule,
     //NoopAnimationsModule
   ],
   providers: [
     // Add your providers here
     // Example: { provide: SomeService, useClass: SomeServiceImpl }
-    
+    //provideAnimations(),
   ],
   templateUrl: './app.component.html',
   //template: `<h1>Hello world!</h1>`,
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  //encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
   // title = 'resumeApp';
